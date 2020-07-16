d67b2d8c56f9e508e1113a67339788ee47308cc8
rijalBinHusen <jeryzal333@gmail.com>
2020-07-16 20:31:23 +0700

    menambah data.js, belum bisa menampilkan

---
+++ data.js
@@ -0,0 +1,31 @@
+let datanya = {"status" : 200, value : [
+	{"id" : "demos_001",
+	 "date" : "2020-01-18 09:07:03",
+	 "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
+	 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+	 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\
+	 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\
+	 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\
+	 proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
+	 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+	 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\
+	 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\
+	 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\
+	 proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+	 {"id" : "demos_002",
+	 "date" : "2019-06-12 18:37:03",
+	 "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
+	 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+	 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\
+	 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\
+	 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\
+	 proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
+	 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+	 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\
+	 consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess\
+	 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\
+	 proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},	
+	 {"id" : "demos_003",
+	 "date" : "2018-04-23 20:57:03",
+	 "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor inci didunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,	 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non roident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+]}
